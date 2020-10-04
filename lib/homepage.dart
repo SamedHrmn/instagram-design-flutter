b90813ba2548bd6e9f2a66e7d3b9000174cf7fd9
SamedHrmn <samed_harman@hotmail.com>
2020-10-04 14:46:00 +0300

    Custom appBar icin MyTopBar sinifi yazildi.

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,7 +9,59 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: MyTopBar(
+          title: "Instagram",
+        ),
+    );
+  }
+}
 
+class MyTopBar extends StatelessWidget with PreferredSizeWidget {
+  final String title;
+  final Color backgroundColor;
+
+  final Size preferredSize;
+
+  MyTopBar({this.title, this.backgroundColor})
+      : preferredSize = Size.fromHeight(50);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Material(
+        elevation: 8,
+        child: Container(
+          height: 50,
+          color: backgroundColor,
+          child: Row(
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              Icon(
+                Icons.camera_alt,
+                color: Colors.black,
+              ),
+              Spacer(),
+              Text(
+                "Instagram",
+                style: TextStyle(fontFamily: "Billabong", fontSize: 28),
+              ),
+              Spacer(),
+              RotationTransition(
+                child: Icon(
+                  Icons.send,
+                  color: Colors.black,
+                ),
+                turns: AlwaysStoppedAnimation(-15 / 360),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
